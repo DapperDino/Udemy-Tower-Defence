9e7f7ceb04497043e3f91c4f14eb72bac173d05a
Nathan <natemi1010@gmail.com>
2020-04-28 16:03:40 +0100

    Creating Enemy

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace DapperDino.TD.Enemies
+{
+    public class Enemy : MonoBehaviour
+    {
+        [SerializeField] private EnemyData enemyData = null;
+    }
+}
+
---
+++ Assets/Scripts/Enemies/EnemyData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace DapperDino.TD.Enemies
+{
+    [CreateAssetMenu(fileName = "New Enemy Data", menuName = "Enemies/Enemy Data")]
+    public class EnemyData : ScriptableObject
+    {
+        [SerializeField] private int damage = 1;
+        [SerializeField] private int health = 50;
+        [SerializeField] private int moneyReward = 20;
+        [SerializeField] private float movementSpeed = 5f;
+
+        public int Damage => damage;
+        public int Health => health;
+        public int MoneyReward => moneyReward;
+        public float MovementSpeed => movementSpeed;
+    }
+}
