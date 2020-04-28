7d5981b2c549e34e22e25f088c603276539dada3
Nathan <natemi1010@gmail.com>
2020-04-28 17:12:19 +0100

    Enemy Movement

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -5,6 +5,44 @@ namespace DapperDino.TD.Enemies
     public class Enemy : MonoBehaviour
     {
         [SerializeField] private EnemyData enemyData = null;
+
+        private Node targetNode;
+        private Vector3 currentDirection;
+
+        private const float MinDistance = 0.1f;
+
+        public EnemyData EnemyData => enemyData;
+
+        private void Update()
+        {
+            if (targetNode == null) { return; }
+
+            MoveToTarget();
+        }
+
+        private void MoveToTarget()
+        {
+            transform.Translate(currentDirection * enemyData.MovementSpeed * Time.deltaTime, Space.World);
+
+            if ((transform.position - targetNode.transform.position).sqrMagnitude < MinDistance * MinDistance)
+            {
+                SetNode(targetNode.NextNode);
+            }
+        }
+
+        public void SetNode(Node nextNode)
+        {
+            targetNode = nextNode;
+
+            if (targetNode == null) { return; }
+
+            currentDirection = (targetNode.transform.position - transform.position).normalized;
+
+            if(currentDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(currentDirection);
+            }
+        }
     }
 }
 
---
+++ Assets/Scripts/Enemies/Node.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace DapperDino.TD.Enemies
+{
+    public class Node : MonoBehaviour
+    {
+        [SerializeField] private Node[] nodes = new Node[0];
+
+        private int nextNodeIndex;
+
+        public Node NextNode
+        {
+            get
+            {
+                if (nodes.Length == 0) { return null; }
+
+                Node nextNode = nodes[nextNodeIndex];
+
+                if (nextNodeIndex + 1 != nodes.Length)
+                {
+                    nextNodeIndex++;
+                }
+                else
+                {
+                    nextNodeIndex = 0;
+                }
+
+                return nextNode;
+            }
+        }
+
+        #region Debug
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+
+            Vector3 myPos = transform.position + new Vector3(0f, 0.5f, 0f);
+
+            Gizmos.DrawWireSphere(myPos, 0.5f);
+
+            foreach (var node in nodes)
+            {
+                Gizmos.DrawLine(myPos, node.transform.position + new Vector3(0f, 0.5f, 0f));
+            }
+        }
+
+        #endregion
+    }
+}
