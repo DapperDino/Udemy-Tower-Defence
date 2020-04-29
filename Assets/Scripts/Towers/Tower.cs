f9be59740f1706f4092b9c02bf68353d10c27917
Nathan <natemi1010@gmail.com>
2020-04-29 15:19:17 +0100

    Tower Tooltip UI

--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace DapperDino.TD.Towers
 {
     public class Tower : MonoBehaviour
     {
         [SerializeField] private TowerData towerData = null;
+
+        public static event Action<TowerHolder> OnTowerSelected;
 
         private TowerHolder towerHolder;
 
@@ -14,5 +17,7 @@ namespace DapperDino.TD.Towers
         {
             this.towerHolder = towerHolder;
         }
+
+        private void OnMouseDown() => OnTowerSelected?.Invoke(towerHolder);
     }
 }
---
+++ Assets/Scripts/Towers/TowerTooltipDisplay.cs
@@ -0,0 +1,45 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace DapperDino.TD.Towers
+{
+    public class TowerTooltipDisplay : MonoBehaviour, IPointerExitHandler
+    {
+        [SerializeField] private TowerShop towerShop = null;
+        [SerializeField] private GameObject tooltipDisplay = null;
+        [SerializeField] private Image towerIconImage = null;
+        [SerializeField] private TMP_Text towerNameText = null;
+        [SerializeField] private TMP_Text towerPriceText = null;
+        [SerializeField] private TMP_Text towerDPSText = null;
+        [SerializeField] private TMP_Text towerRangeText = null;
+
+        private Camera mainCamera;
+        private TowerHolder towerHolder;
+
+        private void OnEnable() => Tower.OnTowerSelected += HandleTowerSelected;
+        private void Start() => mainCamera = Camera.main;
+        private void OnDisable() => Tower.OnTowerSelected -= HandleTowerSelected;
+
+        private void HandleTowerSelected(TowerHolder towerHolder)
+        {
+            tooltipDisplay.transform.position = mainCamera.WorldToScreenPoint(towerHolder.Tower.transform.position);
+
+            towerIconImage.sprite = towerHolder.Tower.TowerData.Icon;
+            towerNameText.text = towerHolder.Tower.TowerData.Name;
+            towerPriceText.text = $"${towerHolder.Tower.TowerData.Price}";
+            towerDPSText.text = $"DPS: {towerHolder.Tower.TowerData.DPS}";
+            towerRangeText.text = $"Range: {towerHolder.Tower.TowerData.Range}";
+
+            this.towerHolder = towerHolder;
+
+            tooltipDisplay.SetActive(true);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            tooltipDisplay.SetActive(false);
+        }
+    }
+}
