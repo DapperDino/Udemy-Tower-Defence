9a6b26e5a799274d40d4af65cb6900c714254976
Nathan <natemi1010@gmail.com>
2020-04-29 14:01:42 +0100

    Damaging The Player

---
+++ Assets/Scripts/Combat/HealthDisplay.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace DapperDino.TD.Combat
+{
+    public class HealthDisplay : MonoBehaviour
+    {
+        [SerializeField] private HealthSystem healthSystem = null;
+        [SerializeField] private TMP_Text healthText = null;
+
+        private void OnEnable()
+        {
+            HandleHealthChanged(healthSystem.Health);
+
+            healthSystem.OnHealthChanged += HandleHealthChanged;
+        }
+
+        private void OnDisable()
+        {
+            healthSystem.OnHealthChanged -= HandleHealthChanged;
+        }
+
+        private void HandleHealthChanged(int health)
+        {
+            healthText.text = health.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Combat/HealthSystem.cs
@@ -0,0 +1,31 @@
+﻿using DapperDino.TD.Enemies;
+using DapperDino.TD.Waves;
+using System;
+using UnityEngine;
+
+namespace DapperDino.TD.Combat
+{
+    public class HealthSystem : MonoBehaviour
+    {
+        [SerializeField] private int health = 100;
+
+        public event Action<int> OnHealthChanged;
+        public static Action OnPlayerLose;
+
+        public int Health => health;
+
+        private void OnEnable() => WaveDestination.OnEnemyReachedEnd += HandleEnemyReachedEnd;
+        private void OnDisable() => WaveDestination.OnEnemyReachedEnd -= HandleEnemyReachedEnd;
+
+        private void HandleEnemyReachedEnd(EnemyData enemyData)
+        {
+            health = Mathf.Max(health - enemyData.Damage);
+
+            OnHealthChanged?.Invoke(health);
+
+            if (health != 0) { return; }
+
+            OnPlayerLose?.Invoke();
+        }
+    }
+}
